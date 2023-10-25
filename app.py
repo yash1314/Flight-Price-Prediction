5dae0ebb5c249e16946209764fd6f47524bd046f
yash1314 <yashkeshari79@gmail.com>
2023-10-25 11:40:48 +0530

    small UI changes

--- app.py
+++ app.py
@@ -4,26 +4,29 @@ import streamlit as st
 import datetime 
 import src.utils as utils
 from src.pipelines.prediction_pipeline import PredictPipeline
+from src.logger import logging
+from src.exception import CustomException
 import time
 
 predict = PredictPipeline()
 
 def main():
+    
     global predict
-
+        
     st.title('Flight Fare Price Prediction.')
-    st.markdown("### Predict ticket price for you flight journey.")
+    st.markdown("Predict ticket price for you flight journey.")
     st.markdown('Enter all the details and then press predict to get price estimation.')
 
     cols1, cols2 , cols3= st.columns(3)
 
     with cols1:
-        st.markdown("### Arival Airport")
-        Source = st.selectbox(label= 'From: ', options= ['New Delhi', 'Banglore', 'Mumbai', 'Chennai'])
+        st.markdown("### From")
+        Source = st.selectbox(label= 'Boarding: ', options= ['New Delhi', 'Banglore', 'Mumbai', 'Chennai'])
 
     with cols3:
-        st.markdown("### Departure Airport")
-        Destination = st.selectbox(label='To', options= ['Cochin', 'Banglore', 'New Delhi', 'Hyderabad'])
+        st.markdown("### To")
+        Destination = st.selectbox(label='Arrival', options= ['Cochin', 'Banglore', 'New Delhi', 'Hyderabad'])
 
     st.markdown('')
 
@@ -32,7 +35,7 @@ def main():
         date = date = st.date_input(label='Enter boarding date',value=datetime.datetime.now(), format = "DD/MM/YYYY" )
 
     with cols3:
-        st.markdown("### Select Airline")
+        st.markdown("### Airline")
         airline = st.selectbox(label= 'Airlines: ', options= [ 'Air India',
         'GoAir',
         'IndiGo',
@@ -47,31 +50,32 @@ def main():
 
 
     with cols1:
-        st.markdown("### Select Flight Duration")
+        st.markdown("### Flight Duration")
         duration = st.selectbox(label= 'Duration of flight: ', options= ['Short', 'Medium', 'Long'])
 
     with cols3:
-        st.markdown("### Select Number Stops")
+        st.markdown("### Number of Stops")
         total_stops = st.selectbox(label = 'Number of Stops', options= ['non-stop', '1 stop', '2 stops', '3 stops', '4 stops'])
 
 
     st.markdown('')
 
-    if st.button('Predict'):
+    if st.button('Calculate'):
         
         input_data = utils.preprocess(Airline= airline, Date_of_Journey=date, Source= Source,
                                         Destination= Destination, Duration=duration, Total_Stops=total_stops)
-        
+        logging.info('User Input data preprocessing complete')
+
         result = np.round(predict.predict(features = input_data))
 
-        bar = st.progress(30)
-        time.sleep(0.5)
-        bar.progress(60)
-        time.sleep(0.25)
-        bar.progress(100)
-        
+        logging.info('Prediction successful')
+
+        with st.spinner('processing'):
+            time.sleep(1)
+        # bar.progress(60)
+        # time.sleep(0.25)
+        # bar.progress(100)
         st.header(f"The Predicted Price of Flight ticket is ₹ {result}")
 
-
-if __name__ =="__main__":
+if __name__ == "__main__":
     main()
--- src/utils.py
+++ src/utils.py
@@ -83,35 +83,39 @@ def convert_to_minutes(duration):
             return None
 
 def preprocess(Airline, Date_of_Journey, Source, Destination, Duration, Total_Stops):
-    
-    my_cols = {'Total_Stops' :0, 'journey_date' :0,'journey_month' :0,'Air Asia' :0,'Air India':0, 'GoAir':0, 'IndiGo':0, 'Jet Airways':0, 'Jet Airways Business':0, 'Multiple carriers':0,
- 'Multiple carriers Premium economy':0, 'SpiceJet':0, 'Vistara':0,
- 'Vistara Premium economy':0, 'Chennai':0, 'Mumbai':0, 'Cochin':0, 'Hyderabad':0, 'New Delhi':0, 'duration':0}
-    
-    new_cols = my_cols.copy()
-
-    my_cols['journey_date'], my_cols['journey_month'] = pd.to_datetime(Date_of_Journey).day, pd.to_datetime(Date_of_Journey).month
-
-    my_cols['duration'] = {'Short': 1, 'Medium': 2, 'Long': 3}.get(Duration, 0)
-    my_cols['Total_Stops'] = {'non-stop' : 0, '1 stop': 1, '2 stops': 2, '3 stops': 3, '4 stops': 4}.get(Total_Stops, 0)
-    
-    if Destination == 'Banglore': 
-        pass
-    else:
-        my_cols[f'{Destination}'] = 1
+    try:
+        my_cols = {'Total_Stops' :0, 'journey_date' :0,'journey_month' :0,'Air Asia' :0,'Air India':0, 'GoAir':0, 'IndiGo':0, 'Jet Airways':0, 'Jet Airways Business':0, 'Multiple carriers':0,
+    'Multiple carriers Premium economy':0, 'SpiceJet':0, 'Vistara':0,
+    'Vistara Premium economy':0, 'Chennai':0, 'Mumbai':0, 'Cochin':0, 'Hyderabad':0, 'New Delhi':0, 'duration':0}
         
-    if Source == 'Banglore':
-        pass
-    else:
-        my_cols[f'{Source}'] = 1
-        
-    if Airline == 'Trujet':
-        pass
-    else:
-        my_cols[f"{Airline}"] = 1
-        
-    
-    df = pd.DataFrame(data = my_cols, index = [0])
-    my_cols = new_cols
+        new_cols = my_cols.copy()
 
-    return df
+        my_cols['journey_date'], my_cols['journey_month'] = pd.to_datetime(Date_of_Journey).day, pd.to_datetime(Date_of_Journey).month
+
+        my_cols['duration'] = {'Short': 1, 'Medium': 2, 'Long': 3}.get(Duration, 0)
+        my_cols['Total_Stops'] = {'non-stop' : 0, '1 stop': 1, '2 stops': 2, '3 stops': 3, '4 stops': 4}.get(Total_Stops, 0)
+        
+        if Destination == 'Banglore': 
+            pass
+        else:
+            my_cols[f'{Destination}'] = 1
+            
+        if Source == 'Banglore':
+            pass
+        else:
+            my_cols[f'{Source}'] = 1
+            
+        if Airline == 'Trujet':
+            pass
+        else:
+            my_cols[f"{Airline}"] = 1
+            
+        
+        df = pd.DataFrame(data = my_cols, index = [0])
+        my_cols = new_cols
+
+        return df
+    
+    except Exception as e:
+        logging.info('Error occured in user input data preprocessing.')
+        raise CustomException(e, sys)
